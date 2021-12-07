f9c275206382cadb9d45c817af65342ca11efa65
dinh trong hiep <hiep.dinh@fssc.com.vn>
2021-12-07 13:12:08 +0700

    API QUYEN

--- RestAPI/Controllers/CorporateActionsController.cs
+++ RestAPI/Controllers/CorporateActionsController.cs
@@ -63,6 +63,143 @@ namespace RestAPI.Controllers
                 return responses;
             }
         }
+        // Su kien quyen chia co tuc bang tien DNSE-1580: DNS.2021.10.1.44 
+        [Route("corporate-actions/cash-dividend")]
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage cashdividend(HttpRequestMessage request)
+        {
+            string preFixlogSession = "corporate-actions/cash-dividend ";
+            Log.Info(preFixlogSession + "======================BEGIN");
+            Bussiness.modCommon.LogFullRequest(request);
 
+            try
+            {
+                if (request.Content.Headers.ContentType == null
+                    || request.Content.Headers.ContentType.MediaType.ToLower() == "application/json")
+                {
+                    string ipaddress = modCommon.getRequestHeaderValue(request, "client-ip");
+
+                    var result = Bussiness.CorporateActionsProcess.cashdividend(request.Content.ReadAsStringAsync().Result, ipaddress);
+                    if (result.GetType() == typeof(BoResponse) && ((BoResponse)result).s == Constants.Result_OK)
+                    {
+                        var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.OK, result);
+                        Log.Info(preFixlogSession + "======================END");
+                        return responses;
+                    }
+                    else
+                    {
+                        var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.BadRequest, result);
+                        Log.Info(preFixlogSession + "======================END");
+                        return responses;
+                    }
+                }
+                else
+                {
+                    var v_err_request = JObject.Parse("{'error': 400, 'message': 'Invalid Input Content-Type'}");
+                    Log.Info(preFixlogSession + "======================END");
+                    return request.CreateResponse(HttpStatusCode.BadRequest, v_err_request);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(preFixlogSession, ex);
+                var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.InternalServerError, ex);
+                Log.Info(preFixlogSession + "======================END");
+                return responses;
+            }
+        }
+        // Su kien quyen co phieu thuong DNSE-1580: DNS.2021.10.1.44 
+        [Route("corporate-actions/stock-bonus")]
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage stockbonus(HttpRequestMessage request)
+        {
+            string preFixlogSession = "corporate-actions/stock-bonus ";
+            Log.Info(preFixlogSession + "======================BEGIN");
+            Bussiness.modCommon.LogFullRequest(request);
+
+            try
+            {
+                if (request.Content.Headers.ContentType == null
+                    || request.Content.Headers.ContentType.MediaType.ToLower() == "application/json")
+                {
+                    string ipaddress = modCommon.getRequestHeaderValue(request, "client-ip");
+
+                    var result = Bussiness.CorporateActionsProcess.stockbonus(request.Content.ReadAsStringAsync().Result, ipaddress);
+                    if (result.GetType() == typeof(BoResponse) && ((BoResponse)result).s == Constants.Result_OK)
+                    {
+                        var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.OK, result);
+                        Log.Info(preFixlogSession + "======================END");
+                        return responses;
+                    }
+                    else
+                    {
+                        var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.BadRequest, result);
+                        Log.Info(preFixlogSession + "======================END");
+                        return responses;
+                    }
+                }
+                else
+                {
+                    var v_err_request = JObject.Parse("{'error': 400, 'message': 'Invalid Input Content-Type'}");
+                    Log.Info(preFixlogSession + "======================END");
+                    return request.CreateResponse(HttpStatusCode.BadRequest, v_err_request);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(preFixlogSession, ex);
+                var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.InternalServerError, ex);
+                Log.Info(preFixlogSession + "======================END");
+                return responses;
+            }
+        }
+        // Su kien quyen quyen mua DNSE-1580: DNS.2021.10.1.44 
+        [Route("corporate-actions/right")]
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage right(HttpRequestMessage request)
+        {
+            string preFixlogSession = "corporate-actions/right ";
+            Log.Info(preFixlogSession + "======================BEGIN");
+            Bussiness.modCommon.LogFullRequest(request);
+
+            try
+            {
+                if (request.Content.Headers.ContentType == null
+                    || request.Content.Headers.ContentType.MediaType.ToLower() == "application/json")
+                {
+                    string ipaddress = modCommon.getRequestHeaderValue(request, "client-ip");
+
+                    var result = Bussiness.CorporateActionsProcess.right(request.Content.ReadAsStringAsync().Result, ipaddress);
+                    if (result.GetType() == typeof(BoResponse) && ((BoResponse)result).s == Constants.Result_OK)
+                    {
+                        var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.OK, result);
+                        Log.Info(preFixlogSession + "======================END");
+                        return responses;
+                    }
+                    else
+                    {
+                        var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.BadRequest, result);
+                        Log.Info(preFixlogSession + "======================END");
+                        return responses;
+                    }
+                }
+                else
+                {
+                    var v_err_request = JObject.Parse("{'error': 400, 'message': 'Invalid Input Content-Type'}");
+                    Log.Info(preFixlogSession + "======================END");
+                    return request.CreateResponse(HttpStatusCode.BadRequest, v_err_request);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(preFixlogSession, ex);
+                var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.InternalServerError, ex);
+                Log.Info(preFixlogSession + "======================END");
+                return responses;
+            }
+        }
     }
 }
