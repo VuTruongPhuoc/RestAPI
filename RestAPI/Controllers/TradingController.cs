6815298e12a6871d02ab9d10374094fafb4f1ad1
MortyPham <trang.phamvuthu@fssc.com.vn>
2022-03-15 13:33:26 +0700

    api dong bo co tuc

--- RestAPI/Controllers/TradingController.cs
+++ RestAPI/Controllers/TradingController.cs
@@ -252,6 +252,53 @@ namespace RestAPI.Controllers
             }
         }
 
+        //DNSE-1656 DNS.2022.03.1.09 API Dong bo co tuc
+        [Route("dividend-order")]
+        [System.Web.Http.HttpPost]
+        public HttpResponseMessage updateCareby(HttpRequestMessage request)
+        {
+            string preFixlogSession = "dividend-order ";
+            Log.Info(preFixlogSession + "======================BEGIN");
+            Bussiness.modCommon.LogFullRequest(request);
+
+            try
+            {
+                if (request.Content.Headers.ContentType == null
+                    || request.Content.Headers.ContentType.MediaType.ToLower() == "application/json")
+                {
+                    string ipaddress = modCommon.getRequestHeaderValue(request, "client-ip");
+
+                    var result = Bussiness.TradingProcess.dividendorder(request.Content.ReadAsStringAsync().Result, ipaddress);
+                    if (result.GetType() == typeof(BoResponse) && ((BoResponse)result).s == Constants.Result_OK)
+                    {
+                        var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.OK, result);
+                        Log.Info(preFixlogSession + "======================END");
+                        return responses;
+                    }
+                    else
+                    {
+                        var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.BadRequest, result);
+                        Log.Info(preFixlogSession + "======================END");
+                        return responses;
+                    }
+                }
+                else
+                {
+                    var v_err_request = JObject.Parse("{'error': 400, 'message': 'Invalid Input Content-Type'}");
+                    Log.Info(preFixlogSession + "======================END");
+                    return request.CreateResponse(HttpStatusCode.BadRequest, v_err_request);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(preFixlogSession, ex);
+                var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.InternalServerError, ex);
+                Log.Info(preFixlogSession + "======================END");
+                return responses;
+            }
+        }
+
         #endregion
     }
 }
