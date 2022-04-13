e4e3675aacaf5ba4815b7c0fe584adc1d25cc81c
MortyPham <trang.phamvuthu@fssc.com.vn>
2022-04-13 09:31:36 +0700

    mã lỗi 400

--- RestAPI/Controllers/LoanController.cs
+++ RestAPI/Controllers/LoanController.cs
@@ -77,7 +77,7 @@ namespace RestAPI.Controllers
                 {
                     string ipaddress = modCommon.getRequestHeaderValue(request, "client-ip");
                     var result = Bussiness.LoanProcess.LoanDrawndown(request.Content.ReadAsStringAsync().Result, ipaddress);
-                    if (result.GetType() == typeof(BoResponse) && ((BoResponse)result).s == Constants.Result_OK)
+                    if (result.GetType() == typeof(BoResponseWithData) && ((BoResponseWithData)result).s == Constants.Result_OK)
                     {
                         var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.OK, result);
                         Log.Info(preFixlogSession + "======================END");
@@ -218,7 +218,7 @@ namespace RestAPI.Controllers
                 {
                     string ipaddress = modCommon.getRequestHeaderValue(request, "client-ip");
                     var result = Bussiness.LoanProcess.LoanPayment(request.Content.ReadAsStringAsync().Result, id, ipaddress);
-                    if (result.GetType() == typeof(BoResponse) && ((BoResponse)result).s == Constants.Result_OK)
+                    if (result.GetType() == typeof(BoResponseWithData) && ((BoResponseWithData)result).s == Constants.Result_OK)
                     {
                         var responses = Bussiness.modCommon.CreateResponseAPI(request, HttpStatusCode.OK, result);
                         Log.Info(preFixlogSession + "======================END");
